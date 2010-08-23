b820a4d5b473562209b48739512ae435f3b96ec9
bero714 <bero714@514e8480-22b6-c4a0-c36e-01b3c35d9870>
2010-08-23 04:51:39 +0000

    Added IntelliBox to OpenSALib3. Also,my PSA language opinion demo is included. If you have understood it,please delete it.

---
+++ trunk/OpenSA3/OpenSALib3/CommandReceiver.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenSALib3.PSA;
+
+namespace OpenSALib3
+{
+    public struct Data
+    {
+        public byte Module;
+        public byte ID;
+        public byte ParameterCount;
+        public byte Unknown;
+        public bint ParameterOffset;
+    }
+    /// <summary>
+    /// This is a class used for receive commands from python
+    /// </summary>
+    public unsafe static class CommandReceiver
+    {
+        private static List<Data> _commandList;//I wanted to use CommandList class, but its constracter demands offsets...
+        private static Dictionary<int,Dictionary<ParameterType,object>> _parameterList;//Key is same to CommandList
+        public static List<Data> CommandList { get { return _commandList; } set { _commandList = value; } }
+        public static  Dictionary<int,Dictionary<ParameterType,object>> Parameter { get { return _parameterList; } set { _parameterList = value; } }
+
+
+        private static void GenericHandler(Data data, List<ParameterType> pList, List<object> values)
+        {
+            _commandList.Add(data);
+            if (pList != null && values != null)
+            {
+                if (pList.Count != values.Count)
+                    throw new Exception("Invalid value count");
+
+                int index = _commandList.Count - 1;
+                Dictionary<ParameterType, object> p = new Dictionary<ParameterType, object>();
+                for (int i = 0; i < pList.Count; i++)
+                    p.Add(pList[i], values[i]);
+                _parameterList.Add(index, p);
+            }
+        }
+
+        public static void STimer(int frames) 
+        {
+            Data data = new Data() { Module = 0, ID = 1, ParameterCount = 1 };
+            List<ParameterType> pList =new List<ParameterType> 
+            { ParameterType.Scalar };
+            List<object> values = new List<object> 
+            { 
+                frames 
+            };
+
+            GenericHandler(data, pList, values);
+        }
+
+        public static void Nop()
+        {
+            Data data = new Data() { Module = 0, ID = 2 };
+            GenericHandler(data, null, null);
+        }
+
+        public static void ATimer(int frames)
+        {
+            Data data = new Data() { Module = 0, ID = 2, ParameterCount = 1 };
+            List<ParameterType> pList = new List<ParameterType>
+            {
+                ParameterType.Scalar
+            };
+            List<object> values = new List<object>
+            {
+                frames
+            };
+
+            GenericHandler(data, pList, values);
+        }
+
+        public static void SetLoop(int iterations)
+        {
+            Data data = new Data() { Module = 0, ID = 4, ParameterCount = 1 };
+            List<ParameterType> pList = new List<ParameterType>
+            {
+                ParameterType.Value
+            };
+            List<object> values = new List<object>
+            {
+                iterations
+            };
+
+            GenericHandler(data, pList, values);
+        }
+    }
+}
---
+++ trunk/OpenSA3/Tabuu/UI/DemoWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using BrawlLib.SSBB.ResourceNodes;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using OpenSALib3.DatHandler;
+using System.Collections.Generic;
+using OpenSALib3.PSA;
+using OpenSALib3;
+namespace Tabuu.UI
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("import clr");
+            sb.AppendLine(@"clr.AddReference(""OpenSALib3"")");
+            sb.AppendLine("from OpenSALib3.CommandReceiver import *");
+            intelliBox1.Text = sb.ToString();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var commandList = new List<Data>();// this is list of commands which is passed to CommandReceiver class
+            var parameters = new Dictionary<int, Dictionary<ParameterType, object>>();//parameters
+
+            //set reference to CommandReceiver class.
+            CommandReceiver.CommandList = commandList;
+            CommandReceiver.Parameter = parameters;
+
+            ScriptEngine engine = Python.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+            ScriptSource source = engine.CreateScriptSourceFromString(intelliBox1.Text);
+            source.Execute(scope);
+            //actual conversation between python to C# will use CommandList class, and Parameter class
+        }
+
+
+    }
+}
