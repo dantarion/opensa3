2485a9a0dd821f30e8e8d5417ab90de1947b23f6
bero714 <bero714@514e8480-22b6-c4a0-c36e-01b3c35d9870>
2010-08-23 04:51:39 +0000

    Added IntelliBox to OpenSALib3. Also,my PSA language opinion demo is included. If you have understood it,please delete it.

---
+++ OpenSA3/OpenSALib3/WPFControls/IntelliBox.xaml.cs
@@ -0,0 +1,285 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using OpenSALib3;
+using System.Reflection;
+
+namespace System.Windows.Controls
+{
+    /// <summary>
+    /// IntelListBox.xaml の相互作用ロジック
+    /// </summary>
+    public partial class IntelliBox:System.Windows.Controls.UserControl
+    {
+        private static readonly string[] keywords = 
+        { "and","as","assert","break","class","continue","def","elif","else","except","finally","for","from",
+            "global","False","if","import","in","is","lambda","None","nonlocal","not","or","pass","raise","return",
+            "Truetry","while","with","yeild"};
+        private static readonly char[] tokens = {'(',')',',',';',':','[',']','{','}',' ','\n','\r','\t'};
+        private bool completed = false;
+        private bool insertIndent = false;
+        private string currentIndent = "";
+        private string colonStack = "";
+        private Dictionary<string, string> descriptions = new Dictionary<string, string>();
+
+        private List<string> _targetSource=new List<string>();
+        public List<string> TargetSource { get { return _targetSource; } set { _targetSource = value; } }
+        public string Text { get { return CodeBox.Text; } set { CodeBox.Text = value; } }
+        private bool useIntelliSense = true;
+        public bool UseIntelliSense { get { return useIntelliSense; } set { useIntelliSense = value; } }
+
+        public IntelliBox()
+        {
+            InitializeComponent();
+            GetMembers();
+        }
+        /// <summary>
+        /// This gets current word.
+        /// </summary>
+        /// <param name="textBox"></param>
+        /// <returns></returns>
+        private static string GetCurrentWord(TextBox textBox)
+        {
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                return string.Empty;
+            }
+            if (textBox.CaretIndex == 0)
+            {
+                return string.Empty;
+            }
+
+            int index = textBox.CaretIndex - 1;
+            int space = textBox.Text.LastIndexOfAny(tokens, index) + 1;
+
+            return textBox.Text.Substring(space, textBox.CaretIndex - space);
+        }
+
+        private bool IsComment()
+        {
+            if (CodeBox.Text.Length > 0)
+            {
+                for (int i = CodeBox.CaretIndex-1; i >= 0; i--)
+                    if (CodeBox.Text[i] == '\n')
+                        return false;
+                    else if (CodeBox.Text[i] == '#')
+                        return true;
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Generates ListViewItem
+        /// </summary>
+        private void GenerateItems()
+        {
+            ListView.Items.Clear();
+
+            string currentWord = GetCurrentWord(CodeBox);
+            string longestWord="";
+            if (!IsComment())
+            {
+                if (!string.IsNullOrWhiteSpace(currentWord))
+                    foreach (string s in _targetSource.Where(x => x.StartsWith(currentWord)&&x!=currentWord))
+                    {
+                        ToolTip tip = new Controls.ToolTip() { Content = descriptions.ContainsKey(s) ? descriptions[s] : null };
+                        ListView.Items.Add(new ListViewItem() { Content = s, ToolTip = tip });
+                        if (s.Length > longestWord.Length)
+                            longestWord = s;
+                    }
+                if (ListView.Items.Count > 0)
+                {
+                    ListView.SelectedIndex = 0;
+                    popup.IsOpen = true;
+                    ListView.Width = longestWord.Length * 10;
+                }
+                else
+                    popup.IsOpen = false;
+            }
+        }
+
+        private void SelectItem()
+        {
+            if (ListView.SelectedItems.Count > 0)
+            {
+                var textBox = popup.PlacementTarget as TextBox;
+                var caretIndex = textBox.CaretIndex;
+                var currentWord = GetCurrentWord(textBox);
+                var selectedText = ((ListViewItem)ListView.SelectedItem).Content as string;
+
+                // 選択されたものを挿入
+                var tmpText = textBox.Text.Remove(caretIndex - currentWord.Length, currentWord.Length);
+                textBox.Text = tmpText.Insert(caretIndex - currentWord.Length, selectedText);
+                textBox.CaretIndex = caretIndex + selectedText.Length;
+                popup.IsOpen = false;
+            }
+        }
+
+        private void GetMembers()
+        {
+            //Get PSAName
+            Type t = typeof(CommandReceiver);
+            MethodInfo[] info = t.GetMethods();
+            foreach (MethodInfo i in info)
+                if (i.IsPublic && !i.Name.Contains("set_") && !i.Name.Contains("get_") && i.DeclaringType == typeof(CommandReceiver))
+                {
+                    _targetSource.Add(i.Name); descriptions.Add(i.Name, i.Name + "(");
+                    ParameterInfo[] param = i.GetParameters();
+                    foreach (ParameterInfo paraminfo in param)
+                        descriptions[i.Name] += paraminfo.ParameterType.Name+ " " + paraminfo.Name+",";
+                    descriptions[i.Name] += ")";
+                }
+
+            foreach (string s in keywords)
+                _targetSource.Add(s);
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, Input.KeyEventArgs e)
+        {
+            if (UseIntelliSense)
+            {
+                Action UpKeyPressed = () =>
+                {
+                    if (popup.IsOpen)
+                    {
+                        if (ListView.SelectedIndex >= 1)
+                            ListView.SelectedIndex = ListView.SelectedIndex - 1;
+                        e.Handled = true; ((ListViewItem)ListView.SelectedItem).Focus();
+                    }
+                };
+                Action DownKeyPressed = () =>
+                {
+                    if (popup.IsOpen)
+                    {
+                        if (ListView.SelectedIndex >= 0 && ListView.SelectedIndex < ListView.Items.Count - 1)
+                        {
+                            ListView.SelectedIndex = ListView.SelectedIndex + 1;
+                            e.Handled = true; ((ListViewItem)ListView.SelectedItem).Focus();
+                        }
+                    }
+                };
+                Action EnterKeyPressed = () =>
+                {
+                    if (popup.IsOpen)
+                    {
+                        CodeBox.AcceptsReturn = false;
+                        SelectItem();
+                        ListView.Items.Clear();
+                        completed = true;
+                    }
+                    else
+                    {
+                        CodeBox.AcceptsReturn = true;
+                        insertIndent = true;
+                        currentIndent = "";
+                        int i = CodeBox.SelectionStart - 1;
+                        if (i >= 0 && CodeBox.Text.Length != 0)
+                        {
+                            while (CodeBox.Text[i] != '\n')//search \n
+                            {
+                                i--;
+                                if (i <= 0)
+                                {
+                                    i = 0; break;
+                                }
+                                if (CodeBox.Text[i] == '\n')
+                                {
+                                    i++; break;
+                                }
+                            }
+                           while (CodeBox.Text[i] == ' ')
+                           {
+                               currentIndent += " "; i++;
+                               if (i == CodeBox.Text.Length || CodeBox.Text[i] != ' ')
+                                   break;
+                           }
+                        }
+                        else
+                            currentIndent = "";
+
+                    }
+                };
+                Action SpaceKeyPressed = () =>
+                {
+                    if (popup.IsOpen)
+                    {
+                        CodeBox.AcceptsReturn = false;
+                        SelectItem();
+                        ListView.Items.Clear();
+                        completed = true;
+                    }
+                };
+                Action Defaultcase = () =>
+                {
+                };
+                Action BackKeyPressed = () =>
+                {
+                };
+                Action ColonKeyPressed = () =>
+                    {
+                        colonStack += "    ";
+                    };
+                switch (e.Key)
+                {
+                    case Key.OemSemicolon: ColonKeyPressed(); break;
+                    case Key.Space: popup.IsOpen = false; SpaceKeyPressed(); break;
+                    case Key.Up: UpKeyPressed(); break;
+                    case Key.Down: DownKeyPressed(); break;
+                    case Key.Back: BackKeyPressed(); break;
+                    case Key.Enter: EnterKeyPressed(); break;
+                    default: Defaultcase(); break;
+                }
+            }
+        }
+
+
+        private void ListView_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return || e.Key == Key.Space)
+            {
+                SelectItem();
+                ListView.Items.Clear();
+                popup.IsOpen = false;
+                CodeBox.Focus();
+                CodeBox.AcceptsReturn = false;
+                completed = true;
+            }
+            else if ((e.Key >= Key.A && e.Key <= Key.Z) || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || (e.Key >= Key.D0 && e.Key <= Key.D9)||e.Key==Key.Back)
+            {
+                CodeBox.Focus();
+            }
+        }
+
+        private void ListView_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            SelectItem();
+            ListView.Items.Clear();
+            popup.IsOpen = false;
+            CodeBox.Focus();
+        }
+
+        private void CodeBox_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (UseIntelliSense)
+            {
+                if (insertIndent)
+                {
+                    int caretIndex = CodeBox.CaretIndex;
+                    CodeBox.Text = CodeBox.Text.Insert(CodeBox.SelectionStart, currentIndent+colonStack);
+                    CodeBox.CaretIndex = caretIndex + currentIndent.Length+colonStack.Length;
+                    colonStack = "";
+                    insertIndent = false;
+                }
+                if (!completed)
+                {
+                    GenerateItems();
+                    popup.PlacementTarget = CodeBox;
+                    popup.PlacementRectangle = CodeBox.GetRectFromCharacterIndex(CodeBox.CaretIndex);
+                }
+                else
+                    completed = false;
+            }
+        }
+    }
+}
+
